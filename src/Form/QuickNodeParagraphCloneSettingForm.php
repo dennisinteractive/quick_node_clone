33764497e7a5e31c4943b2e10dd2d6ac92275dec
Tasha Harrison <tasha_harrison@dennis.co.uk>
2018-02-13 16:10:54 +0000

    Case 29367; remove config fix to add to its own issue and branch

--- src/Form/QuickNodeParagraphCloneSettingForm.php
+++ src/Form/QuickNodeParagraphCloneSettingForm.php
@@ -141,7 +141,6 @@ class QuickNodeParagraphCloneSettingForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $form_state->cleanValues();
     $formvalues = $form_state->getValues();
     foreach ($formvalues['para'] as $key => $values) {
       if (empty($value)) {
