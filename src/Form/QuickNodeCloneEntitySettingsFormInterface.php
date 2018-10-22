86001ac44ae3c10ded9aa93cfcaf720ee56dd8b7
Marcelo Vani <marcellovani@yahoo.co.uk>
2018-10-22 17:42:42 +0100

    Renamed files

---
+++ src/Form/QuickNodeCloneEntitySettingsFormInterface.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\quick_node_clone;
+
+/**
+ * Provides an interface for an Clone Entity forms.
+ */
+interface QuickNodeCloneEntitySettingsFormInterface {
+  /**
+   * Sets the entity type the settings form is for.
+   *
+   * @param $entityTypeId
+   *    The entity type Id i.e. article
+   *
+   * @return mixed
+   */
+  public function setEntityType($entityTypeId);
+
+  /**
+   * Returns the entity type Id. i.e. article
+   *
+   * @return mixed
+   */
+  public function getEntityTypeId();
+
+  /**
+   * The array of config names.
+   *
+   * @return mixed
+   */
+  public function getEditableConfigNames();
+
+  /**
+   * Returns the bundles for the entity.
+   *
+   * @return mixed
+   */
+  public function getEntityBundles();
+
+  /**
+   * Returns the selected bundles on the form.
+   *
+   * @param $form_state
+   *
+   * @return mixed
+   */
+  public function getSelectedBundles($form_state);
+
+  /**
+   * Returns the description field.
+   *
+   * @param $form_state
+   *
+   * @return mixed
+   */
+  public function getDescription($form_state);
+
+  /**
+   * Returns the default fields.
+   *
+   * @param $value
+   *
+   * @return mixed
+   */
+  public function getDefaultFields($value);
+
+  /**
+   * Return the configuration.
+   *
+   * @param $value
+   *
+   * @return mixed
+   */
+  public function getSettings($value);
+}
